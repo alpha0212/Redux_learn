f2937d5dece4ce1ece5cd8d75118fae703f59009
alpha0212 <qkrcjfgml321@naver.com>
2022-10-31 23:44:45 +0900

    Next learn to do list redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
-import { legacy_createStore as createStore } from "redux";
+import { createStore } from "redux";
+const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+/**
+ * ADD 와 MINUS를 함수화 해주는 이유는 handle이나 modify할 때
+ * string형식에서 오타가 나면 오류를 잡지 못하는데
+ * 함수화하면 함수명에 오타가 생겼을 시에 undefined가 뜨기 때문에
+ * 오류를 잡을수 있다.
+ */
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+//Next learn to do list redux
